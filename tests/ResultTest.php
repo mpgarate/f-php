976fba44453bc7dfc78cc8ef8069aa2c6fc6fbe2
michael <mpgarate@gmail.com>
2017-02-28 10:50:40 -0600

    adjust result test

--- tests/ResultTest.php
+++ tests/ResultTest.php
@@ -7,16 +7,9 @@ use PHPUnit\Framework\TestCase;
 
 use FPHP\Result;
 
-function divide(int $x, int $y): Result {
-    if ($y === 0) {
-        return Result::error('divide by zero');
-    }
-
-    return Result::ok($x / $y);
-}
-
 class ErrTest extends TestCase {
-    public function testErr() {
-        $this->assertEquals(Result::ok(1.0), divide(4, 4));
+    public function testOk() {
+        $result = Result::ok(1.0);
+        $this->assertEquals(Result::ok(1.0), $result);
     }
 }
